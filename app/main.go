3356574c2eeb3eb7eba88f0a4a9d52e0aecbdb0d
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2021-12-14 22:49:22 +0600

    cors origin issue should be fixed

--- app/main.go
+++ app/main.go
@@ -8,8 +8,25 @@ import (
 
 func main() {
 	r := gin.Default()
+	router.Use(CORSMiddleware())
 	// r.GET("/ping", handler.TestApi())
 	r.POST("/addskills", handler.AddSkills())
 	r.GET("/getskills", handler.GETAllSkills())
 	r.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Content-Type", "application/json")
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Max-Age", "86400")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max, X-Auth-Secret, Uid, Aid, CToken")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
 }
